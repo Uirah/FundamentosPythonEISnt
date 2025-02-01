8e3ac1722ad87af87552b1e33abc71ead6d28d10
Uirá Silva Duarte <106848619+Uirah@users.noreply.github.com>
2025-02-01 17:42:26 +0000

    arrumado

--- UFCD10793_FT01/FT03/ex09.py
+++ UFCD10793_FT01/FT03/ex09.py
@@ -8,27 +8,27 @@ idade = int(input("Digite sua idade em anos: "))
 peso = float(input("Digite seu peso em Kg: "))
 altura = float(input("Digite altura em metros: "))
 
-imc = peso/(altura*altura)
+imc = round((peso/(altura*altura)), 2)
 
 if imc < 17:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Muito abaixo do peso ideal \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Muito abaixo do peso ideal \n")
 elif 17 <= imc < 18.5:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Abaixo do peso \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Abaixo do peso \n")
     
 elif 18.5 <= imc < 25:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Peso normal \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Peso normal \n")
     
 elif 25 <= imc < 30:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Acima do peso \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Acima do peso \n")
     
 elif 30 <= imc < 35:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Obesidade I \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Obesidade I \n")
     
 elif 35 <= imc < 40:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Obesidade II (severa)  \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Obesidade II (severa)  \n")
     
 elif 40 <= imc:
-    print(f"{nome}, teu peso resulta em um IMC de {imc:.2f}, por isso é considerado como: Obesidade III (mórbida) \n")
+    print(f"{nome}, seu peso resulta em um IMC de {imc}, isso é considerado como: Obesidade III (mórbida) \n")
 
 else:
     print(f"Deu ruim :( \n")
